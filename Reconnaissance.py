de51ec4b3f59a9f23e52cb3e3f743d32c5d52f93
Fellipemarcal <136606878+Fellipemarcal@users.noreply.github.com>
2024-05-29 10:16:20 -0300

    Add files via upload

---
+++ Reconnaissance.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+# Load the Haar cascade file
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Load the face recognizer model
+#recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Load the trained data
+#recognizer.read('trained_data.yml')
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Loop through all detected faces
+    for (x, y, w, h) in faces:
+        # Extract the face ROI
+        face_roi = gray[y:y+h, x:x+w]
+
+        # Recognize the face
+        #label, confidence = recognizer.predict(face_roi)
+
+        # Draw the bounding box of the face along with the recognized name and confidence
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.putText(frame, 'Name: fellipe, status: étudiant', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    # Display the results
+    cv2.imshow('Visage', frame)
+
+    # Exit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and destroy all windows
+cap.release()
+cv2.destroyAllWindows()
